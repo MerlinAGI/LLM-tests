a81ac9b6f0165bb1ea1abb35945fab3c18f241aa
Florian Juengermann <florianjuengermann@gmail.com>
2023-02-25 18:56:23 -0800

    added feedback

--- llm-tests-frontend/src/components/Results.tsx
+++ llm-tests-frontend/src/components/Results.tsx
@@ -49,6 +49,16 @@ export default function Results({ test }: Props) {
         >
           {test.completion}
         </Card>
+        {test.judgement?.status === "failed" && (
+          <Card
+            size="small"
+            title="Feedback"
+            bordered={false}
+            style={{ width: 300 }}
+          >
+            {test.judgement.text}
+          </Card>
+        )}
         <Button type="primary" onClick={() => null}>
           {test.judgement?.status !== null ? "Run again" : "Run"}
         </Button>
