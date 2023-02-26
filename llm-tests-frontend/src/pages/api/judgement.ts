5c2dfdf9b5d64ee22bab3c69f4b6376640541791
Alfred Wahlforss <alfred@wahlforss.com>
2023-02-25 18:49:07 -0800

    added claude

--- llm-tests-frontend/src/pages/api/judgement.ts
+++ llm-tests-frontend/src/pages/api/judgement.ts
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 const { Configuration, OpenAIApi } = require("openai");
+const { GenerateApi, GenerateApiApiKeys } = require("vellum-client-node");
+
+const generate = new GenerateApi();
+generate.setApiKey(GenerateApiApiKeys.apiKeyAuth, process.env.VELLUM_API_KEY);
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -17,15 +21,21 @@ export default async function handler(
 ) {
   try {
     const prompt = JSON.parse(req.body).prompt;
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      max_tokens: 100,
-      stop: ["JUDGEMENT", "#"],
-      temperature: 0,
+    // const completion = await openai.createCompletion({
+    //   model: "text-davinci-003",
+    //   prompt: prompt,
+    //   max_tokens: 100,
+    //   stop: ["JUDGEMENT", "#"],
+    //   temperature: 0,
+    // });
+    const generation = await generate.generate({
+      deploymentName: "merlin2",
+      requests: [{ inputValues: { input: prompt } }],
     });
 
-    res.status(200).json({ text: completion.data.choices[0].text });
+    res
+      .status(200)
+      .json({ text: generation.body.results[0].data.completions[0].text });
   } catch (error) {
     console.log(error);
     res.status(500).json({ text: error ? "Error" : "No error" });
